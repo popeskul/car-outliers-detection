78297b3cc0c24ae86ea09981e95afffad5262490
Pavlo Popeskul <pavlo.popeskul@gmail.com>
2024-01-10 17:47:10 +0200

    refactor(outlier_service.go): extract outlier check logic to separate function for better readability feat(outlier_service.go): implement concurrent outlier detection using goroutines and sync package to improve performance fix(outlier_service.go): handle potential errors in concurrent outlier detection using error channel to ensure robustness

--- internal/services/outlier_service.go
+++ internal/services/outlier_service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/popeskul/car-outliers-detection/internal/domain"
 	"github.com/popeskul/car-outliers-detection/internal/utils"
+	"sync"
 )
 
 //go:generate mockgen -destination=mocks/mock_outlier_service.go -package=mocks github.com/popeskul/car-outliers-detection/internal/services IOutlierService
@@ -32,23 +33,55 @@ func (s *OutlierService) DetectOutliers(machines []*domain.Machine) ([]*domain.M
 		return nil, fmt.Errorf("machines is empty")
 	}
 
-	maxAgeMonths := 100 * 12 // Максимальный допустимый возраст в месяцах (например, 100 лет)
+	maxAgeMonths := 100 * 12 // Maximum acceptable age in months (e.g., 100 years)
 
 	var outliers []*domain.Machine
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+	errChan := make(chan error, 1)
+
 	for _, machine := range machines {
-		if !machine.Validate() {
-			return nil, fmt.Errorf("invalid machine: %v", machine)
-		}
+		wg.Add(1)
+		go func(machine *domain.Machine) {
+			defer wg.Done()
 
-		ageInMonths, valid := utils.ConvertToMonths(machine.Age)
-		if !valid {
-			return nil, fmt.Errorf("invalid age format: %s", machine.Age)
-		}
+			isOutlier, err := checkMachineOutlier(machine, maxAgeMonths)
+			if err != nil {
+				select {
+				case errChan <- err: // Send error to channel, non-blocking
+				default: // If channel already has an error, do not block
+				}
+				return
+			}
 
-		if ageInMonths > maxAgeMonths {
-			outliers = append(outliers, machine)
-		}
+			if isOutlier {
+				mutex.Lock()
+				outliers = append(outliers, machine)
+				mutex.Unlock()
+			}
+		}(machine)
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	// Check if there were any errors
+	if err, ok := <-errChan; ok {
+		return nil, err
 	}
 
 	return outliers, nil
+}
+
+func checkMachineOutlier(machine *domain.Machine, maxAgeMonths int) (bool, error) {
+	if !machine.Validate() {
+		return false, fmt.Errorf("invalid machine: %v", machine)
+	}
+
+	ageInMonths, valid := utils.ConvertToMonths(machine.Age)
+	if !valid {
+		return false, fmt.Errorf("invalid age format: %s", machine.Age)
+	}
+
+	return ageInMonths > maxAgeMonths, nil
 }
